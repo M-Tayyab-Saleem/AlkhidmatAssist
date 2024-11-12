803e502788f0e0162a5588f2f8cd20af2cd43610
Muhammad Salman <154021273+SalmanO7@users.noreply.github.com>
2024-11-12 17:43:00 +0500

    navbar work

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React from "react";
+import NavLogo from "../assets/navLogo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-      <h2>Hellow World</h2>
-    </div>
-  )
-}
+    <nav className="flex justify-around items-center w-full ">
+      <img src={NavLogo} alt="Nav Logo" className="w-10" />
+      <ul className="flex justify-center items-center gap-5">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About us</a>
+        </li>
+        <li>
+          <a href="/application">Applications</a>
+        </li>
+        <li>
+          <a href="/contact">Contact us</a>
+        </li>
+      </ul>
 
-export default Navbar
+      <div className="btns flex justify-center items-center gap-3">
+        <a href="">Complain</a>
+        <a href="">Donate</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
